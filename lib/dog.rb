82d6f89c2f3321c6051a2cb0cc4ed629a97a5d30
Xochitl Menjivar <xandra.menji@gmail.com>
2018-05-31 05:28:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog 
   
   def name=(dogs_name)
-    @this_dogs_name = dogs_name 
+    @name = dogs_name 
   end
   
   def name 
-    @this_dogs_name
+    @name
   end
 
   def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
+    @breed = dogs_breed
   end
   
   def breed
-    @this_dogs_breed
+    @breed
   end
   
 end
