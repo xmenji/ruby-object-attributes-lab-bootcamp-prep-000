3eebda6709da4d3ffbd2e0526eca95728f596274
Xochitl Menjivar <xandra.menji@gmail.com>
2018-05-31 05:27:27 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(persons_name)
+    @this_persons_name = persons_name
+  end
+  
+  def name 
+    @this_persons_name
+  end
+  
+  def job=(persons_job)
+    @this_persons_job = persons_job
+  end
+  
+  def job
+    @this_persons_job
+  end
+  
+end
